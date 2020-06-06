a5ee97abbb34e53c80b285a1eb3704a01d7736ee
Mohit Varma <mv501049@gmail.com>
2020-06-06 12:19:06 +0530

    animate a container

--- lib/ui/second_page.dart
+++ lib/ui/second_page.dart
@@ -1,8 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatefulWidget {
   final String title;
-
 
   SecondPage({this.title});
 
@@ -11,24 +12,69 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  double _height = 50;
+  double _width = 50;
+
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadiusGeometry = BorderRadius.circular(8);
+  Duration duration = const Duration(seconds: 1);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_arrow),
+        onPressed: () {
+          setState(() {
+            final random = Random.secure();
+
+            // Generate a random width and height.
+            _width = random.nextInt(300).toDouble();
+            _height = random.nextInt(300).toDouble();
+
+            // Generate a random color.
+            _color = Color.fromRGBO(
+              random.nextInt(256),
+              random.nextInt(256),
+              random.nextInt(256),
+              1,
+            );
+
+            // Generate a random border radius.
+            _borderRadiusGeometry =
+                BorderRadius.circular(random.nextInt(100).toDouble());
+          });
+        },
+      ),
       backgroundColor: Colors.grey,
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: MaterialButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          color: Colors.deepPurple,
-          onLongPress: () {
-            debugPrint('long pressed');
-          },
-          splashColor: Colors.white,
-          child: Text('First Page', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+        child: AnimatedContainer(
+          height: _height,
+          width: _width,
+          child: MaterialButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            color: Colors.deepPurple,
+            onLongPress: () {
+              debugPrint('long pressed');
+            },
+            splashColor: Colors.white,
+            child: Text(
+              'First Page',
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadiusGeometry,
+          ),
+          duration: duration,
+          curve: Curves.ease,
         ),
       ),
     );
