d6c805c3bc90b606871183319563d251f9d129f6
Mohit Varma <mv501049@gmail.com>
2020-06-07 09:10:34 +0530

    fade in and out

--- lib/ui/second_page.dart
+++ lib/ui/second_page.dart
@@ -14,7 +14,7 @@ class SecondPage extends StatefulWidget {
 class _SecondPageState extends State<SecondPage> {
   double _height = 50;
   double _width = 50;
-
+  bool visible = false;
   Color _color = Colors.green;
   BorderRadiusGeometry _borderRadiusGeometry = BorderRadius.circular(8);
   Duration duration = const Duration(seconds: 1);
@@ -26,6 +26,9 @@ class _SecondPageState extends State<SecondPage> {
         child: Icon(Icons.play_arrow),
         onPressed: () {
           setState(() {
+
+            visible = !visible;
+
             final random = Random.secure();
 
             // Generate a random width and height.
@@ -46,14 +49,17 @@ class _SecondPageState extends State<SecondPage> {
           });
         },
       ),
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: AnimatedContainer(
-          height: _height,
-          width: _width,
+        child: AnimatedOpacity(
+          opacity: visible ? 1.0 : 0.0,
+          duration: const Duration(milliseconds: 500),
+          child: AnimatedContainer(
+            height: _height,
+            width: _width,
 //          child: MaterialButton(
 //            onPressed: () {
 //              Navigator.of(context).pop();
@@ -69,12 +75,13 @@ class _SecondPageState extends State<SecondPage> {
 //              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
 //            ),
 //          ),
-          decoration: BoxDecoration(
-            color: _color,
-            borderRadius: _borderRadiusGeometry,
+            decoration: BoxDecoration(
+              color: _color,
+              borderRadius: _borderRadiusGeometry,
+            ),
+            duration: duration,
+            curve: Curves.ease,
           ),
-          duration: duration,
-          curve: Curves.ease,
         ),
       ),
     );
