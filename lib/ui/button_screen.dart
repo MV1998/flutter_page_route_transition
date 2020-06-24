d94c8da2691f9b9269ecfc767c9582b088803dda
Mohit Varma <mv501049@gmail.com>
2020-06-24 19:06:41 +0530

    final commit.

--- lib/ui/button_screen.dart
+++ lib/ui/button_screen.dart
@@ -7,74 +7,76 @@ class CustomButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          FlatButton(
-            onPressed: () {},
-            child: Text(
-              'Access',
-              style: TextStyle(color: Colors.white),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            FlatButton(
+              onPressed: () {},
+              child: Text(
+                'Access',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.deepPurple,
+              splashColor: Colors.amber,
+              shape: BeveledRectangleBorder(
+                  side: BorderSide(
+                      color: Colors.black12, width: 1, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(10)),
             ),
-            color: Colors.deepPurple,
-            splashColor: Colors.amber,
-            shape: BeveledRectangleBorder(
-                side: BorderSide(
-                    color: Colors.black12, width: 1, style: BorderStyle.solid),
-                borderRadius: BorderRadius.circular(10)),
-          ),
-          RaisedButton(
-            color: Colors.deepPurple,
-            onPressed: () {},
-            child: Text(
-              'Access',
-              style: TextStyle(color: Colors.white),
+            RaisedButton(
+              color: Colors.deepPurple,
+              onPressed: () {},
+              child: Text(
+                'Access',
+                style: TextStyle(color: Colors.white),
+              ),
+              splashColor: Colors.amber,
+              shape: BeveledRectangleBorder(
+                  side: BorderSide(
+                      color: Colors.black12, width: 1, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(10)),
             ),
-            splashColor: Colors.amber,
-            shape: BeveledRectangleBorder(
-                side: BorderSide(
-                    color: Colors.black12, width: 1, style: BorderStyle.solid),
-                borderRadius: BorderRadius.circular(10)),
-          ),
-          FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {},
-            splashColor: Colors.amber,
-          ),
-          IconButton(
-            iconSize: 40,
-            icon: Icon(Icons.security),
-            onPressed: () {},
-          ),
-          InkWell(
-            child: Container(
-              padding: EdgeInsets.all(12.0),
-              child: Text('Flat Button'),
+            FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {},
+              splashColor: Colors.amber,
             ),
-            splashColor: Colors.amber,
-            onTap: () {},
-            borderRadius: BorderRadius.circular(10),
-            customBorder: BeveledRectangleBorder(
-                side: BorderSide(
-                    color: Colors.black12, width: 1, style: BorderStyle.solid),
-                borderRadius: BorderRadius.circular(10)),
-          ),
-          GestureDetector(
-            onTap: () {
-              Scaffold.of(context).showSnackBar(SnackBar(
-                content: Text('GestureDetector'),
-              ));
-            },
-            child: Icon(
-              Icons.security,
-              size: 40,
+            IconButton(
+              iconSize: 40,
+              icon: Icon(Icons.security),
+              onPressed: () {},
             ),
-          ),
-          CupertinoButton(
-            child: Text('Apple'),
-            onPressed: () {},
-          ),
-          SentryExample()
-        ],
+            InkWell(
+              child: Container(
+                padding: EdgeInsets.all(12.0),
+                child: Text('Flat Button'),
+              ),
+              splashColor: Colors.amber,
+              onTap: () {},
+              borderRadius: BorderRadius.circular(10),
+              customBorder: BeveledRectangleBorder(
+                  side: BorderSide(
+                      color: Colors.black12, width: 1, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            GestureDetector(
+              onTap: () {
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text('GestureDetector'),
+                ));
+              },
+              child: Icon(
+                Icons.security,
+                size: 40,
+              ),
+            ),
+            CupertinoButton(
+              child: Text('Apple'),
+              onPressed: () {},
+            ),
+            SentryExample()
+          ],
+        ),
       ),
     );
   }
